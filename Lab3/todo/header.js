8dab955ee6ec8d74712d7562d8313d6e7979f01e
zbatraliyeva <36437293+zbatraliyeva@users.noreply.github.com>
2018-03-01 12:12:17 +0600

    Add files via upload

---
+++ Lab3/todo/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default class Header extends React.Component {
+  render(){
+    return (
+    <h2>Todo list</h2>
+  );
+  }
+}
---
+++ Lab3/todo/todoItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './todoItem.css';
+
+export default class TodoItem extends React.Component {
+  removeTodo(id) {
+    this.props.removeTodo(id);
+  }
+  render(){
+    return(
+      <div className="todoWrapper">
+<button className="removeTodo" onClick={(e)=>this.removeTodo(this.props.id)}>remove</button>{this.props.todo.text}
+      </div>
+
+
+    );
+  }
+}
